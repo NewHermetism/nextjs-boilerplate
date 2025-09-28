76368cc3a3e0c7fc729323c3ad62a5b0759e929c
NewHermetism <sarkteus@gmail.com>
2025-09-27 20:40:46 -0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,29 @@
 import react from '@vitejs/plugin-react';
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import { resolve } from 'path';
+import { Alias } from 'vite';
 import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import svgrPlugin from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
+
+const aliasWithIndex = (name: string): Alias[] => [
+  {
+    find: new RegExp(`^${name}$`),
+    replacement: resolve(__dirname, `src/${name}/index.ts`)
+  },
+  {
+    find: new RegExp(`^${name}/`),
+    replacement: `${resolve(__dirname, `src/${name}`)}/`
+  }
+];
+
+const aliasDirOnly = (name: string): Alias[] => [
+  {
+    find: new RegExp(`^${name}/`),
+    replacement: `${resolve(__dirname, `src/${name}`)}/`
+  }
+];
 
 export default defineConfig({
   server: {
@@ -28,6 +48,24 @@ export default defineConfig({
       globals: { Buffer: true, global: true, process: true }
     })
   ],
+  resolve: {
+    alias: [
+      ...aliasDirOnly('assets'),
+      ...aliasWithIndex('components'),
+      ...aliasWithIndex('config'),
+      ...aliasDirOnly('games'),
+      ...aliasWithIndex('helpers'),
+      ...aliasWithIndex('hooks'),
+      ...aliasWithIndex('localConstants'),
+      ...aliasWithIndex('pages'),
+      ...aliasWithIndex('routes'),
+      ...aliasWithIndex('services'),
+      ...aliasDirOnly('styles'),
+      ...aliasWithIndex('types'),
+      ...aliasWithIndex('utils'),
+      ...aliasWithIndex('wrappers')
+    ]
+  },
   build: {
     outDir: 'build'
   },
